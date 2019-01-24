4be4cac308fa36528fd2c0090ae07c444eb9b653
Stepan Telitsyn <telitsyn@scand.com>
2019-01-24 17:10:14 +0300

    [#20] Decorators: update decorators

---
+++ src/util/decorator.ts
@@ -0,0 +1,23 @@
+export function createDecorator (
+  factory: (target: any, key: string) => void
+): PropertyDecorator {
+  return function (target: any, key: string) {
+    const descriptor = arguments[2];
+
+    if (descriptor) {
+      delete descriptor.initializer;
+      descriptor.writable = true;
+    }
+
+    const Ctor = typeof target === 'function'
+      ? target
+      : target.constructor;
+    if (!Ctor.__decorators__) {
+      Ctor.__decorators__ = [];
+    }
+
+    Ctor.__decorators__.push(function (options) {
+      return factory(options, key);
+    });
+  };
+}
