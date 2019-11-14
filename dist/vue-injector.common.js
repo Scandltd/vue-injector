84b1798df2e8d164f4d44aeea5d1601df9d201ed
Stepan Telitsyn <telitsyn@scand.com>
2019-11-14 16:24:03 +0300

    [build] 2.2.1

--- dist/vue-injector.common.js
+++ dist/vue-injector.common.js
@@ -1,5 +1,5 @@
 /*!
-  * @scandltd/vue-injector v2.2.0
+  * @scandltd/vue-injector v2.2.1
   * (c) 2019 Scandltd
   * @license GPL-2.0
   */
@@ -1616,7 +1616,7 @@ var VueInjector = /** @class */ (function () {
     return VueInjector;
 }());
 VueInjector.install = install;
-VueInjector.version = '2.2.0';
+VueInjector.version = '2.2.1';
 if (inBrowser && window.Vue) {
     window.Vue.use(VueInjector);
 }
