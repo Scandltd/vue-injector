03653ffc27ba025fbc703e187b66bd6720d4981d
Stepan Telitsyn <telitsyn@scand.com>
2019-11-16 13:03:53 +0000

    refactor: change docker config

---
+++ .docker/node/startup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+git config --global user.name "$GIT_USERNAME"
+git config --global user.email "$GIT_EMAIL"
+
+git config --global core.autocrlf false
+git config --global core.eol lf
+
+$@
