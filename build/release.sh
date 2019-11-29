361f5b5e22bda27b11f409502fb89eab490a7a9b
Roman Rapchinsky <rapchinsky@scand.com>
2019-11-29 15:59:37 +0300

    chore: change release script to ask if need to push

--- build/release.sh
+++ build/release.sh
@@ -4,26 +4,60 @@ set -e
 echo "Enter release version: "
 read VERSION
 
-release() {
-  echo "Releasing $VERSION ..."
-  yarn test
-  VERSION=$VERSION yarn run build
-
-  # commit
+commit() {
   git add dist/*
   git commit -m "[build] $VERSION"
   yarn version --new-version $VERSION
+}
 
-  # publish
-  git push origin --tags
+publish() {
+  git push $1 --tags
   git push
 }
 
-while true; do
+confirmRelease() {
   read -p "Releasing $VERSION - are you sure? (y/n)" yn
   case $yn in
     [Yy]* ) release; break;;
     [Nn]* ) exit;;
     * ) echo "Please answer yes or no.";;
   esac
+}
+
+confirmRemote() {
+  linenum=1
+  remotes=(origin)
+  r=$(git remote)
+
+  echo "Remotes:"
+  while read remote ; do
+    remotes+=($remote)
+    echo $linenum: $remote
+    linenum=$((linenum+1))
+  done <<< "$r"
+
+  read -p "What remote do you want to use? " remotenum
+  publish ${remotes[$remotenum]}
+}
+
+confirmPush() {
+  read -p "Do you want to push release to remote? (y/n)" yn
+  case $yn in
+    [Yy]* ) confirmRemote; break;;
+    [Nn]* ) exit;;
+    * ) echo "Please answer yes or no.";;
+  esac
+}
+
+release() {
+  echo "Releasing $VERSION ..."
+  yarn test
+  VERSION=$VERSION yarn run build
+
+  commit
+  confirmPush
+}
+
+while true; do
+  confirmRelease
 done
