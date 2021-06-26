492eb4acb5f851e160e23151993e6fee16ce5bf6
Stepan Telitsyn <telitsyn@scand.com>
2021-06-26 13:37:56 +0300

    refactor: add build versions

--- build/configs.js
+++ build/configs.js
@@ -9,5 +9,13 @@ module.exports = [
     file: 'vue-injector.min.js',
     format: 'umd',
     env: 'production'
+  },
+  {
+    file: 'vue-injector.common.js',
+    format: 'commonjs'
+  },
+  {
+    file: 'vue-injector.esm.js',
+    format: 'commonjs-module'
   }
 ];
